804146506d6990ab6cf1f95d406246a241c96d0f
connorkoch0511 <connor.koch@student.csulb.edu>
2023-08-21 18:52:42 -0700

    Fix typo in fillRect()

--- s1.js
+++ s1.js
@@ -2,4 +2,4 @@ let can = document.getElementById("table");
 let draw_ = can.getContext('2d');
 
 draw_.fillStyle = "black";
-draw_.fillReact(0,0,can.clientWidth,can.height);
+draw_.fillRect(0,0,can.clientWidth,can.height);
